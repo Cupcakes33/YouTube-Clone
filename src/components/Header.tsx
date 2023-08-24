d1c475a855628649c502f8a0cc6b688ddb2f31ef
Cupcakes33 <rute930627@gmail.com>
2023-08-24 17:30:43 +0900

    :sparkles: Feat : 헤더 컴포넌트 1차 완성

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,10 @@
 import SearchInput from "./SearchInput";
 import LogoIcon from "./icons/LogoIcon";
 
+const DUMMY_USER_PROFILE_IMAGE_URL =
+  "https://plus.unsplash.com/premium_photo-1690366910332-6cb1dc10c000?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIzfHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60";
+// 차후에 유저 인증, 로그인 기능을 구현하면 이 부분을 수정할 것.
+
 export default function Header() {
   return (
     <header className="w-full h-[56px] px-4 flex flex-row items-center justify-between border-b">
@@ -15,11 +19,16 @@ export default function Header() {
           KR
         </span>
       </a>
+      
       {/* Search Input */}
       <SearchInput />
+      
       {/* User Menu */}
       <div>
-        <img/>
+        <img
+          src={DUMMY_USER_PROFILE_IMAGE_URL}
+          className="w-8 h-8 rounded-full"
+        />
       </div>
     </header>
   );

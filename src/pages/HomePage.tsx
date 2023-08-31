7b5a7ff6e6fa2c7797d356746fdbadb0fca24d0c
Cupcakes33 <rute930627@gmail.com>
2023-08-31 20:40:43 +0900

    :sparkles: Feat : Home 컴포넌트 무한스크롤 임시구현
    정상적으로 작동하지 않는 경우가 있어서 개선요망

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import VideoItem from "../components/VideoItem";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useYoutubeApi } from "../context/YoutubeApiContext";
 import { FetchVideos } from "../service/youtubeInstance";
 
@@ -9,34 +9,62 @@ const CATEGORYS = ["0", "10", "15", "17", "20"];
 export default function HomePage() {
   const [category, setCategory] = useState("0");
   const { youtubeInstance } = useYoutubeApi();
-  const {
-    data: { items, nextPageToken } = {},
-    isLoading,
-    isError,
-  } = useQuery<FetchVideos>(
-    ["videos", category],
-    () => youtubeInstance.fetchVideos({ categoryId: category }),
-    {
-      staleTime: 1000 * 60 * 10,
+
+  const { data, fetchNextPage, hasNextPage, isLoading, isError } =
+    useInfiniteQuery<FetchVideos>(
+      ["videos", category],
+      ({ pageParam }) =>
+        youtubeInstance.fetchVideos({
+          categoryId: category,
+          pageToken: pageParam,
+        }),
+      {
+        getNextPageParam: (lastPage) => lastPage.nextPageToken,
+        staleTime: 1000 * 60 * 10,
+      }
+    );
+
+  const observer = useRef<IntersectionObserver | null>(null);
+  const loadMoreRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (isLoading || !hasNextPage) return;
+    if (observer.current) observer.current.disconnect();
+
+    observer.current = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          fetchNextPage();
+        }
+      },
+      { threshold: 1 }
+    );
+
+    if (loadMoreRef.current) {
+      observer.current.observe(loadMoreRef.current);
     }
-  );
+  }, [isLoading, hasNextPage]);
+  console.log(data);
 
   return (
     <>
       {isLoading && <div>Loading...</div>}
       {isError && <div>Error...</div>}
-      {CATEGORYS.map((category) => (
+      {CATEGORYS.map((cat) => (
         <button
-          key={category}
+          key={cat}
           className="p-2 border mr-2"
-          onClick={() => setCategory(category)}
+          onClick={() => setCategory(cat)}
         >
-          {category}
+          {cat}
         </button>
       ))}
-      {items
-        ? items.map((video) => <VideoItem key={video.id} video={video} />)
-        : null}
+      <div className="p-5 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
+        {data?.pages.map((page) =>
+          page.items.map((video) => <VideoItem key={video.id} video={video} />)
+        )}
+      </div>
+      <div ref={loadMoreRef} />
     </>
   );
 }
